9dc076005f273fd38f73d5dd110028ed6c6ea938
Rob Bos <rajbos@users.noreply.github.com>
2021-09-08 13:45:19 +0200

    Check if target branch exists and create it (#4)
    Check if the target branch already exists and create it if not

---
+++ checkBranch.js
@@ -0,0 +1,96 @@
+/**
+ * API: https://docs.github.com/en/rest/reference/repos#get-a-branch
+ */
+ const axios = require('axios')
+ const path = require('path')
+ const core = require('@actions/core')
+ 
+ async function checkBranch(
+   { Authorization, username, repo, branchName }
+ ) {
+     
+   // load api url from context
+   let BASE_URL = process.env.GITHUB_API_URL
+   core.debug(`Using API url: ${BASE_URL}`)
+   core.debug(`Checking if branch exists with name ${branchName} in repo ${repo}`)
+ 
+   const url =
+     BASE_URL +
+     path.posix.join(
+       `/repos/${repo}/branches`
+     )
+   core.debug(`Request URL: ${url}`)
+   // load all branches, since we need the info about the default branch as well
+   const res = await axios({
+     method: 'get',
+     url,
+     responseType: 'application/json',
+     headers: {
+       Authorization,
+       'Content-Type': 'application/json'
+     }
+   })
+   .then(({data}) => { 
+     // result succesful
+     let jsonResult = JSON.stringify(data);
+     core.debug(`Succesful API call with result: ${jsonResult}`)
+     return { data: data } 
+    })
+   .catch(err => {
+     if (err.toString() !== 'Error: Request failed with status code 404') {
+       console.log(err)
+     }
+     // errors should not happen
+     return { data: { } }
+   })
+   let branches = res.data
+
+   let json = JSON.stringify(branches);
+   core.debug(`Branches: ${json}`)
+
+   if (branches == null) {
+       core.debug(`Error loading existing branches from API`)
+       return null
+   }
+
+   // check if the branch name already exists
+   let branch = branches.find(branch => { branch.name === branchName })
+
+   if (branch) {
+    core.debug(`Branch with name ${branchName} already exists, continuing as normal`)
+   }
+   else {
+    core.debug(`Need to create a new branch first with name ${branchName}`)
+    let defaultBranch = branches[0]
+    console.log(`Found default branch to branch of: ${defaultBranch.name} with sha: ${defaultBranch.commit.sha}`)
+
+    let branchCreateUrl = BASE_URL +
+      path.posix.join(
+        `/repos/${repo}/git/refs`
+      )
+    core.debug(`Request URL to create new branch: ${branchCreateUrl}`)
+
+    return axios({
+        method: 'post',
+        url: branchCreateUrl,
+        responseType: 'application/json',
+        headers: {
+        Authorization,
+        'Content-Type': 'application/json'
+        },
+        data: {
+            ref: `refs/heads/${branchName}`,
+            sha: defaultBranch.commit.sha
+        }
+    }).then(({ data }) => {
+        // return non empty object to check on
+        console.log(`created new branch with ref: ${data.ref} based on ${defaultBranch.name}`)
+        return { }
+    }).catch(err => {
+        console.log(`Error creating the branch with name ${branchName} and sha ${defaultBranch.commit.sha}: ${error}`)
+        return null
+    })
+    }
+ }
+ module.exports = checkBranch
+ 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const upload = require('./upload.js')
 const core = require('@actions/core')
+const checkBranch = require('./checkBranch.js')
 const inputPath = core.getInput('file-path')
 const inputRemoteDir = core.getInput('remote-dir')
 const inputUsername = core.getInput('username')
@@ -14,10 +15,30 @@ core.debug('Input username: ' + inputUsername)
 core.debug('Input repo: ' + inputRepo)
 core.debug('Input commitMessage: ' + commitMessage)
 core.debug('Input branchName: ' + branchName)
+
 if (!fs.existsSync(inputPath)) {
   core.setFailed(`filePath doesn't exist: ${inputPath}`)
   return
 }
+
+async function createBranchIfNotExists() {
+  let branchResult = await checkBranch({
+    Authorization: `Bearer ${core.getInput('access-token')}`,
+    username: inputUsername,
+    repo: inputRepo,
+    branchName
+  })
+
+  if (branchResult == null) {
+    core.setFailed('Error checking or creating the branch')  
+    return false
+  }
+  else {
+    core.debug('Branch result: ' + branchResult)
+    return true
+  }
+}
+
 const isInputPathDir = fs.lstatSync(inputPath).isDirectory()
 const localDir = isInputPathDir ? inputPath : ''
 
@@ -42,6 +63,19 @@ const filePaths = isInputPathDir ? getAllFilePaths(inputPath) : [inputPath]
 core.debug(`filePaths: ${filePaths}`)
 
 async function uploadAll() {
+
+  try {
+    let branchExists = await createBranchIfNotExists()
+    if (!branchExists) {
+      core.setFailed(error)
+      return
+    }
+  } catch (error) {
+    // break off further execution
+    core.setFailed(error)
+    return
+  }
+
   for (let index = 0; index < filePaths.length; index++) {
     const curPath = filePaths[index]
     const remotePath = path.join(
